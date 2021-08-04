e2109040118bac0171f5abc4b0c2b49509c00310
Jean George <jean.george@leanonsystems.com>
2021-08-04 08:53:30 -0300

    Adding my first blog

---
+++ blog-strapi/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b58f6689a8d89b1ed4165f224347dd40'),
+    },
+  },
+});
---
+++ blog-strapi/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e23a66fc-d284-482f-b50c-fa85e86e1c8c'
+};
